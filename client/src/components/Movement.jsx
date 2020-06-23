719c92dc29fb13b4c996ea7892529387d3f7febd
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-06-23 18:57:11 -0500

    Successfully show button on Movement pages for adding an event. Only shows on MOvements that the  logged in User is the creator of

--- client/src/components/Movement.jsx
+++ client/src/components/Movement.jsx
@@ -131,6 +131,17 @@ const Movement = ({
       </div>
       <div className="m-8">
         <div>
+          {/* conditionally render event button if user is the creator of the movement */}
+          {leading && (
+            <div>
+              <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-red-400 rounded shadow m-4" onClick={followMovement}>
+                Schedule Event
+              </button>
+              <br />
+            </div>
+          )}
+
+
           {/* conditionally render follow button if user is logged in */}
           {user && (
             <div>
