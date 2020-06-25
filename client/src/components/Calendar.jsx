b176a1c9490fe043bc627415355b7093af0797ec
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-06-25 14:24:44 -0500

    Clean up the dummy events on the calendar. Settle the sizing of the claendar in the dialog window

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -6,33 +6,26 @@ import moment from 'moment';
 const localizer = momentLocalizer(moment);
 
 const myEventsList = [{
-  title: 'DTS STARTS',
-  start: new Date(2016, 2, 13, 0, 0, 0),
-  end: new Date(2016, 2, 20, 0, 0, 0),
-},
-
-{
-  title: 'DTS ENDS',
-  start: new Date(2016, 10, 6, 0, 0, 0),
-  end: new Date(2016, 10, 13, 0, 0, 0),
-},
-
-{
-  title: 'Some Event',
-  star: new Date(2015, 3, 9, 0, 0, 0),
-  end: new Date(2015, 3, 9, 0, 0, 0),
+  'title': 'DTS STARTS',
+  'start': new Date(2020, 7, 13, 0, 0, 0),
+  'end': new Date(2020, 7, 20, 0, 0, 0),
 },
 {
-  title: 'Conference',
-  start: new Date(2015, 3, 11),
-  end: new Date(2015, 3, 13),
-  desc: 'Big conference for important people',
+  'title': 'DTS ENDS',
+  'start': new Date(2020, 10, 6, 0, 0, 0),
+  'end': new Date(2020, 10, 13, 0, 0, 0),
 },
 {
-  title: 'Meeting',
-  start: new Date(2015, 3, 12, 10, 30, 0, 0),
-  end: new Date(2015, 3, 12, 12, 30, 0, 0),
-  desc: 'Pre-meeting meeting, to prepare for the meeting',
+  'title': 'Conference',
+  'start': new Date(2021, 3, 11),
+  'end': new Date(2021, 3, 13),
+  'desc': 'Big conference for important people',
+},
+{
+  'title': 'Meeting',
+  'start': new Date(2021, 3, 12, 10, 30, 0, 0),
+  'end': new Date(2021, 3, 12, 12, 30, 0, 0),
+  'desc': 'Pre-meeting meeting, to prepare for the meeting',
 }];
 
 const MyCalendar = () => (
@@ -42,7 +35,7 @@ const MyCalendar = () => (
       events={myEventsList}
       startAccessor="start"
       endAccessor="end"
-      style={{ height: 500, width: 800 }}
+      style={{ height: 550, width: 900 }}
     />
   </div>
 );
