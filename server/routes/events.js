79ca6c85dddec1bca0a09920423f1f1ee685a8a5
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-06-27 10:49:20 -0500

    Create event specific route handling on the server side

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,67 +1,77 @@
-// const { Router } = require('express');
-// const {
-//   addEvent,
-//   getEvent,
-//   getAllEvents,
-//   addRSVPCount,
-// } = require('../db/methods');
+const { Router } = require('express');
+const {
+  addEvent,
+  getEvent,
+  getAllEventsForMovement,
+  addRSVP,
+  addRSVPCount,
+} = require('../db/methods');
 
-// const eventRouter = Router();
+const eventRouter = Router();
 
-// // this route will get all the events for the eventLists
-// eventRouter.get('/:movId/:eventId', (req, res) => {
-//   // get all the events from the database
-//   getAllEvents()
-//     .then(movements => {
-//       res.send(movements);
-//     })
-//     .catch(err => {
-//       console.error(err);
-//       res.sendStatus(500);
-//     });
-// });
+// this route will get all the events for the eventLists
+eventRouter.get('/movement/:id', (req, res) => {
+  const { id } = req.params || {};
+  // unstring the id to a number and remove the colon
+  const moveId = parseFloat(id.slice(1));
+  // get all the events from the database
+  getAllEventsForMovement(moveId)
+    .then(events => {
+      res.send(events);
+    })
+    .catch(err => {
+      console.error('Error GETting the events for a movement:', err);
+      res.sendStatus(500);
+    });
+});
 
-// // movementRouter.get('/:id', (req, res) => {
-// //   // this route will get the clicked on movement by the id
-// //   const { id } = req.params || {};
-// //   // unstring the id to a number and remove the colon
-// //   const movementId = parseFloat(id.slice(1));
-// //   // method that get the movement by a particular id from the db
-// //   getMovement(movementId)
-// //     .then(movement => {
-// //       res.send(movement);
-// //     })
-// //     .catch(err => {
-// //       console.error(err);
-// //       res.sendStatus(500);
-// //     });
-// // });
+// this route will get the clicked on event by the id
+eventRouter.get('/:id', (req, res) => {
+  const { id } = req.params || {};
+  // unstring the id to a number and remove the colon
+  const eventId = parseFloat(id.slice(1));
+  // method that get the event by a particular id from the db
+  getEvent(eventId)
+    .then(event => {
+      res.send(event);
+    })
+    .catch(err => {
+      console.error('Error GETting single event by Id:', err);
+      res.sendStatus(500);
+    });
+});
 
-// eventRouter.post('/', (req, res) => {
-//   const { eventObj, id } = req.body;
-//   // add an event to db
-//   addEvent(eventObj, id)
-//     .then(event => {
-//       res.send(event);
-//     })
-//     .catch(err => {
-//       console.error('Error adding the new event', err);
-//     });
-// });
+eventRouter.post('/', (req, res) => {
+  const { eventObj } = req.body;
+  // add an event to db
+  addEvent(eventObj)
+    .then(event => {
+      res.send(event);
+    })
+    .catch(err => {
+      console.error('Error POSTing the new event:', err);
+    });
+});
 
-// eventRouter.post('/RSVPCount/', (req, res) => {
-//   const { id } = req.body;
-//   const eventId = parseFloat(id);
+eventRouter.post('/rsvp/:eventId/:userId', (req, res) => {
+  let { userId, eventId } = req.body;
+  userId = parseFloat(userId);
+  eventId = parseFloat(eventId);
+  addRSVP(userId, eventId)
+    .then(() => {
+      addRSVPCount(eventId)
+        .then(() => {
+          res.sendStatus(200);
+        })
+        .catch(err => {
+          console.error('Error POSTing to RSVPCount for event:', err);
+        });
+    })
+    .catch(() => {
 
-//   addRSVPCount(eventId)
-//     .then(() => {
-//       res.sendStatus(200);
-//     })
-//     .catch(err => {
-//       console.error(err);
-//     });
-// });
+    });
+});
 
-// module.exports = {
-//   eventRouter,
-// };
+module.exports = {
+  eventRouter,
+};
