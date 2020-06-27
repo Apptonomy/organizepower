9fe8bf623bffa870a46343fecb2130c6461c40f9
jessicatorres-debug <jes.tores2277.jt@gmail.com>
2020-06-27 13:06:05 -0500

    Set up the custom date and time pickers, set up the select for category event type and the associated descriptions to show below. Add the appBar to match the full window dialog for the calendar view. Successfuly pull form the time, date, and category inpiuts to update the 'state'

--- client/src/components/CreateEvent.jsx
+++ client/src/components/CreateEvent.jsx
@@ -1,14 +1,23 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
+// Imports to create custom time and date pickers using moment as the utility library
+import { MuiPickersUtilsProvider, KeyboardDatePicker, KeyboardTimePicker } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
+
+// Imports to create dialogs using material-ui and provide page transition functionality
 import { makeStyles } from '@material-ui/core/styles';
 import CloseIcon from '@material-ui/icons/Close';
 import {
   Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle,
   FormControl, FormControlLabel, InputLabel, MenuItem, Select, Switch,
-  Divider, AppBar, Toolbar, IconButton, Typography, Slide,
+  Divider, AppBar, Toolbar, IconButton, Typography, Grid,
 } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
+  appBar: {
+    backgroundColor: '#718582',
+  },
   form: {
     display: 'flex',
     flexDirection: 'column',
@@ -27,68 +36,154 @@ const useStyles = makeStyles((theme) => ({
 const EventCreateDialog = () => {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
+  const [name, setName] = useState('');
+  const [location, setLocation] = useState('');
+  const [selectedTime, setSelectedTime] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [category, setCategory] = useState('');
+  const [description, setDescription] = useState('');
+  const [imageUrl, setImage] = useState('');
+
   const [fullWidth, setFullWidth] = useState(true);
-  const [maxWidth, setMaxWidth] = useState('sm');
+  const [categoryText, setCategoryText] = useState('Select the type of Event you are holding');
 
   const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleSave = () => {
-    axios.post('/event', {
-      
-    });
+    console.log('date', selectedDate);
+    console.log('time', selectedTime);
+    console.log('name', name);
+    console.log('location', location);
+    console.log('category', category);
+    // axios.post('/event', {
+    //   // name: DataTypes.STRING,
+    //   // location: DataTypes.STRING,
+    //   // category: DataTypes.STRING,
+    //   // description: DataTypes.TEXT,
+    //   // rsvpCount: DataTypes.INTEGER,
+    //   // imageUrl: DataTypes.STRING,
+    //   name,
+    //   location,
+    //   time: selectedTime,
+    //   date: selectedDate,
+    //   category,
+    //   description,
+    //   rsvpCount: 0,
+    //   imageUrl,
+    // });
     setOpen(false);
+  };
+
+  const handleCategoryChange = (value) => {
+    setCategory(value);
+    if (value === 'Informational Session') {
+      setCategoryText('Come learn more about the cause.');
+    } else if (value === 'Spread Awareness') {
+      setCategoryText('Help us educate others on the cause.');
+    } else if (value === 'March') {
+      setCategoryText('Participate in a mobile protest.');
+    } else if (value === 'Sit In') {
+      setCategoryText('Participcate in a stationary protest.');
+    } else if (value === 'Other') {
+      setCategoryText('Type of Event not listed here? Give followers some extra details in the description of your event.');
+    }
   };
 
   const handleClose = () => {
     setOpen(false);
   };
 
-  const handleMaxWidthChange = (event) => {
-    setMaxWidth(event.target.value);
-  };
-
-  const handleFullWidthChange = (event) => {
-    setFullWidth(event.target.checked);
-  };
-
   return (
     <>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open max-width dialog
+      <Button variant="outlined" className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-red-400 rounded shadow m-4" onClick={handleClickOpen}>
+        Schedule Event
       </Button>
+
+      {/* <Dialog open={open} onClose={handleClose}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+              <CloseIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Sound
+            </Typography>
+            <Button autoFocus color="inherit" onClick={handleClose}>
+              save
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Dialog> */}
+
       <Dialog
         fullWidth={fullWidth}
-        maxWidth={maxWidth}
+        maxWidth="md"
         open={open}
         onClose={handleClose}
         aria-labelledby="max-width-dialog-title"
       >
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+              <CloseIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              SCHEDULE EVENT
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
         <DialogTitle id="max-width-dialog-title">Optional sizes</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            You can set my maximum width and whether to adapt or not.
-          </DialogContentText>
+          {/* Provide the time and date custom created pickers in grid block */}
+          <MuiPickersUtilsProvider utils={MomentUtils}>
+            <Grid container justify="space-around">
+              <KeyboardDatePicker
+                margin="normal"
+                id="date-picker-dialog"
+                format="MM/dd/yyyy"
+                value={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
+                KeyboardButtonProps={{
+                  'aria-label': 'change date',
+                }}
+              />
+              <KeyboardTimePicker
+                margin="normal"
+                id="time-picker"
+                value={selectedTime}
+                onChange={(time) => setSelectedTime(time)}
+                KeyboardButtonProps={{
+                  'aria-label': 'change time',
+                }}
+              />
+            </Grid>
+          </MuiPickersUtilsProvider>
+          {/* Provide the select field for the event's type /category value */}
           <form className={classes.form} noValidate>
             <FormControl className={classes.formControl}>
-              <InputLabel htmlFor="max-width">maxWidth</InputLabel>
+              <InputLabel htmlFor="max-width">Event Type</InputLabel>
               <Select
-                autoFocus
-                value={maxWidth}
-                onChange={handleMaxWidthChange}
+                value={category}
+                onChange={(event) => {
+                  handleCategoryChange(event.target.value);
+                }}
                 inputProps={{
-                  name: 'max-width',
-                  id: 'max-width',
+                  name: 'category',
+                  id: 'category',
                 }}
               >
-                <MenuItem value={false}>false</MenuItem>
-                <MenuItem value="come learn more about the cause">INFO SESSION</MenuItem>
-                <MenuItem value="help educate others on the cause">RAISE AWARENESS</MenuItem>
-                <MenuItem value="participate in a mobile protest">MARCH</MenuItem>
-                <MenuItem value="participcate in a stationary protest">SIT IN</MenuItem>
-                <MenuItem value="other">OTHER</MenuItem>
+                <MenuItem value="Informational Session">INFO SESSION</MenuItem>
+                <MenuItem value="Spread Awareness">SPREAD AWARENESS</MenuItem>
+                <MenuItem value="March">MARCH</MenuItem>
+                <MenuItem value="Sit in">SIT IN</MenuItem>
+                <MenuItem value="Other">OTHER</MenuItem>
               </Select>
+              <DialogContentText>
+                {categoryText}
+              </DialogContentText>
             </FormControl>
           </form>
         </DialogContent>
