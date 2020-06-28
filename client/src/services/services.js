07cd74072d5a0997af4255af0b99c8b62b7972b1
NaseerHines <Hinesnaseer@gmail.com>
2020-06-28 17:19:26 -0500

    db function to getAll users from db

--- client/src/services/services.js
+++ client/src/services/services.js
@@ -4,6 +4,10 @@ const getUserByUsername = (username) => {
   return axios.get(`/profile/:${username}`);
 };
 
+const getAllUsers = () => {
+  return axios.get('profile/all/users');
+};
+
 const getMovements = () => {
   return axios.get('/movement');
 };
@@ -21,6 +25,16 @@ const getUserProfileById = (id) => {
   return axios.get(`/profile/:${id}`);
 };
 
+// Gets all messages between two users
+const getAllUserMessages = (sender_id, recipient_id) => {
+  return axios.get(`messages/${recipient_id}/from/${sender_id}`);
+};
+
+// post a message to the db that references two users
+const sendUserMessage = (sender_id, recipient_id, messageObj) => {
+  return axios.post(`messages/${sender_id}/to/${recipient_id}`, messageObj);
+};
+
 // Authentication
 
 const login = (username, password) => {
@@ -36,11 +50,14 @@ const signup = (user) => {
 };
 
 export {
+  getAllUsers,
   getUserByUsername,
   getUserProfileById,
   getMovements,
   getMovementsLeading,
   getMovementsFollowing,
+  getAllUserMessages,
+  sendUserMessage,
   logout,
   login,
   signup,
